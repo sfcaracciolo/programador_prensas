7ec4fe8eca903d24f0f5d78451de4e211130ab23
scaracciolo <santiago.caracciolo@gmail.com>
2024-02-20 11:05:53 -0300

    remove files

--- sql/caipe_server_header.sql
+++
@@ -1,7 +0,0 @@
-DROP DATABASE IF EXISTS caipe_server;
-CREATE DATABASE IF NOT EXISTS caipe_server DEFAULT CHARACTER SET 'cp850';
-CREATE USER IF NOT EXISTS 'prod_soft'@localhost IDENTIFIED BY '111111';
-GRANT ALL PRIVILEGES ON caipe_server.* TO 'prod_soft'@localhost;
-GRANT FILE ON *.* TO 'prod_soft'@localhost;
-FLUSH PRIVILEGES;
-USE caipe_server;
